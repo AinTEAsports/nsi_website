10009b1e8b0ad2dea559ecb94697c5fa197c5ea0
AinTEAsports <benkirat.khalil@gmail.com>
2022-03-11 20:53:19 +0100

    Added files and folders

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-function printChoice() {
-    var choice = document.getElementById('selectInput');
+const games = {
+    "morpion" : "/morpion/index.html",
+    "pendu" : "/pendu/index.html",
+    "shifumi" : "/shifumi/index.html"
+};
 
-    document.getElementById('textInput').innerHTML = choice.value;
+
+function printChoice() {
+    var choice = document.getElementById('selectInput').value;
+
+    if (choice == 'Menu Principal') {
+        return;
+    }
+
+    document.getElementById('textInput').innerHTML = choice;
+}
+
+
+function getGameName() {
+    var gameName = document.getElementById('selectInput').value;
+
+    if (gameName == 'Menu Principal') {
+        return null;
+    }
+
+    return gameName.toLowerCase();
+}
+
+
+function gameRedirect() {
+    var gameName = getGameName();
+    
+    if (gameName == null) {
+        window.alert("[!] You need to select a game [!]");
+        return;
+    }
+
+    var gameLink = games[gameName];
+    window.location.href = gameLink;
 }
