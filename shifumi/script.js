17035e84be19f01802125d3caf0d8b24b7d90717
AinTEAsports <benkirat.khalil@gmail.com>
2022-03-13 11:13:35 +0100

    Some changes

--- shifumi/script.js
+++ shifumi/script.js
@@ -13,17 +13,20 @@ function ordiRandomChoice() {
 
 
 function adjustScore() {
-    var userChoice = document.getElementById('userChoice');
+    var userChoice = document.getElementById('userChoice').value;
     var ordiChoice = document.getElementById('ordiChoice');
 
-    if (ordiChoice == userChoice) {
-        document.getElementById('winner') = "Egalite !";
-    } else if ((ordiChoice == "Pierre" && userChoice == "Feuille") ||
-        (ordiChoice == "Feuille" && userChoice == "Ciseau") ||
-        (ordiChoice == "Ciseau " && userChoice == "Pierre")) {
-        document.getElementById('winner') = "User wins !";
+    console.log(userChoice);
+    console.log(ordiChoice);
+
+    if ((ordiChoice === "Pierre" && userChoice === "Ciseau") || (ordiChoice === "Feuille" && userChoice === "Ciseau") || (ordiChoice === "Ciseau" && userChoice === "Pierre")) {
+        document.getElementById('winner').innerHTML = "User won !";
+    } else if ((ordiChoice === "Pierre" && userChoice === "Ciseau") || (ordiChoice === "Feuille" && userChoice === "Pierre") || (ordiChoice === "Ciseau" && userChoice === "Feuille")) {
+        document.getElementById('winner').innerHTML = "Ordi won !";
+    } else if ((ordiChoice === userChoice)) {
+        document.getElementById('winner').innerHTML = "Tie !";
     } else {
-        document.getElementById('winner') = "Ordi wins !";
+        document.getElementById('winner').innerHTML = "!!! PROBLEM !!!";
     }
 }
 
