430d467428305158568f47270bf202ce75d6b6f3
AinTEAsports <benkirat.khalil@gmail.com>
2022-03-20 09:54:40 +0100

    Some changes

--- enigmes/script.js
+++ enigmes/script.js
@@ -7,21 +7,24 @@ var enigmes = {
     "Quand je suis content je me lève, mais je suis plus petit que le reste, qui suis-je ?" : "Le pouce",
     "J'ai un pied mais pas de chaussure, j'ai un chapeau mais pas de tête, qui suis-je ?" : "Un champignon",
     "Plus je suis present et moins je pese, qui suis-je ?" : "Le vide",
+    "Je suis meilleur que Dieu, pire que le diable, les pauvres l'ont, les riches en ont besoin, si tu me mange tu meurt, qui suis-je ?" : "Rien"
 };
 
 
 function ordiRandomChoice() {
     // Je fais une liste avec les elements pierre feuille et ciseau
     // et j'en choisis un au hasard
-    var questionList = Object.values(enigmes);
+    var questionList = Object.keys(enigmes);
+    var reponseList = Object.values(enigmes);
     var randomIndex = (Math.floor(Math.random() * questionList.length));
 
-    return questionList[randomIndex], randomIndex;
+    return questionList[randomIndex], reponseList[randomIndex];
 }
 
 
 function generateEnigme() {
-    var enigme, index = ordiRandomChoice();
+    var enigme, reponse = ordiRandomChoice();
+    console.log(enigme + reponse + "\n");
     document.getElementById('enigme').innerHTML = enigme;
 }
 
