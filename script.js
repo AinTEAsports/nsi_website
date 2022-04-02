9963391b019b36f526467b450a697f2167c90e2b
AinTEAsports <benkirat.khalil@gmail.com>
2022-04-02 13:33:38 +0200

    Some changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Dictionnaire avec la liste des jeux et leur lien
 const games = {
-    "enigmes" : "/enigmes/index.html",
-    "shifumi" : "/shifumi/index.html",
-    "pendu" : "/pendu/index.html",
-    "morpion" : "/morpion/index.html",
+    "enigmes": "/enigmes/index.html",
+    "shifumi": "/shifumi/index.html",
+    "pendu": "/pendu/index.html",
+    "morpion": "/morpion/index.html",
 };
 
 
@@ -22,7 +22,7 @@ function getGameName() {
 // Fonction du bouton pour rediriger vers le lien quand le bouton est pressé
 function gameRedirect() {
     var gameName = getGameName();
-    
+
     if (gameName == null) {
         window.alert("[!] You need to select a game [!]");
         return;
@@ -36,3 +36,33 @@ function gameRedirect() {
 function rickroll() {
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
+
+
+/*
+
+TOUT CA C'EST POUR LE SELECT
+
+*/
+
+/*Dropdown Menu*/
+$('.dropdown').click(function() {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function() {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function() {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+});
+/*End Dropdown Menu*/
+
+
+$('.dropdown-menu li').click(function() {
+    var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</strong>',
+        msg = '<span class="msg">Hidden input value: ';
+    $('.msg').html(msg + input + '</span>');
+});
